85fa10b9d6f5177eedbf467c0ff9528d9f8ad3ae
rohit <rohiitrb@gmail.com>
2025-02-09 23:34:02 +0530

    added Learn More

--- client/src/components/Home.svelte
+++ client/src/components/Home.svelte
@@ -135,7 +135,7 @@
 								handleLearnMoreClick(event.eventUrl)}
 							class="inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors duration-300"
 						>
-							Learn More
+							GET TICKETS
 						</button>
 					</div>
 				</div>
